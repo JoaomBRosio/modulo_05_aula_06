bb1890521a2ec18eff8214c31eb2d3cfb928aff5
melon musk <125475444+JoaomBRosio@users.noreply.github.com>
2023-02-28 16:08:49 -0300

    Add files via upload

---
+++ aula_06/index.php
@@ -0,0 +1,29 @@
+<?php
+
+//Arrays
+
+//$nome[] = 'João';
+//$nome[] = 'Pedro';
+
+//$nome[0] = 'João';
+//$nome[100]= 'João'; 
+
+//$variaveis = ['João', 'pedro', 'davi'];
+//echo $variaveis[0];
+
+//$variaveis = array ('João', 16, true, 10.95);
+
+
+$info['nome'] = 'João'; 
+$info['idade'] = 16;
+$info['estado'] = 'sp';
+$info['partido'] = 22;
+
+echo $info['nome'];
+echo '<br / >';
+echo $info['idade'];
+echo '<br / >';
+echo $info['estado'];
+echo '<br / >'; 
+echo $info['partido'];
+?>
